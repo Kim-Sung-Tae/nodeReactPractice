18512f4dc42a7bfc90642d3a16295366d846aa1f
kst <zyzuzizozz@naver.com>
2022-03-24 17:40:23 +0900

    dbconfig추가

---
+++ front/src/Component/ClassTag.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ClassTag extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ClassTag;
---
+++ front/src/Component/FunctionTag.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FunctionTag() {
+
+
+    
+  return (
+    <div>FunctionTag</div>
+  )
+}
+
+export default FunctionTag
